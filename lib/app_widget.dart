86288e9911121f5c9af50c7fbcc14b6c50eaf784
Emerson Cavalcanti <emerson.cavalanti@escolatecnicalimoeiro.com.br>
2026-05-21 21:01:01 -0300

    instanciando o dio e distribuindo via injecao de dependencias com o provider

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,22 +1,33 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:projetoterceiro/data/http/http_client.dart';
 import 'package:projetoterceiro/pages/home/home_page.dart';
 import 'package:projetoterceiro/pages/login/login.dart';
 import 'package:projetoterceiro/pages/splash_screen.dart';
+import 'package:provider/provider.dart';
 
 class AppWiget extends StatelessWidget {
   const AppWiget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Consumo de API',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SplashScreen(),
-        '/login': (context) => Login(),
-        '/home': (context) => HomePage(),
-      },
+    final dio = Dio();
+
+    return MultiProvider(
+      providers: [
+        Provider<Dio>.value(value: dio),
+        ProxyProvider<Dio, DioClient>(update: (context, dio, dioClient) => DioClient(dio)),
+      ],
+      child: MaterialApp(
+        title: 'Consumo de API',
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => SplashScreen(),
+          '/login': (context) => Login(),
+          '/home': (context) => HomePage(),
+        },
+      ),
     );
   }
 }
